bcb310d700ff758c2fbe8c967677ecb6e794fcfe
Kazoob <>
2024-11-20 19:47:55 -0600

    Add more upgrades and building purchases to auto purchase function.

--- clicker.py
+++ clicker.py
@@ -37,7 +37,7 @@ UPGRADES_ALLOWED = {
 }
 
 PURCHASE_AUTO_MINUTES = 15
-PURCHASE_AUTO_UPGRADES = 10
+PURCHASE_AUTO_UPGRADES = 20
 PURCHASE_AUTO_BUILDINGS_BULK = 90
 PURCHASE_AUTO_BUILDINGS = 20
 
@@ -487,6 +487,10 @@ class Clicker:
                 self.purchase_upgrade(PURCHASE_AUTO_UPGRADES)
 
                 # Purchase buildings.
+                self.purchase_building(PURCHASE_AUTO_BUILDINGS_BULK, True)
+                self.purchase_building(PURCHASE_AUTO_BUILDINGS_BULK, True)
+                self.purchase_building(PURCHASE_AUTO_BUILDINGS_BULK, True)
+                self.purchase_building(PURCHASE_AUTO_BUILDINGS_BULK, True)
                 self.purchase_building(PURCHASE_AUTO_BUILDINGS_BULK, True)
                 self.purchase_building(PURCHASE_AUTO_BUILDINGS, False)
 
